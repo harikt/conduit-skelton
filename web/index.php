2385f2cddff0ea9da9ed5dc8b99cf9ce7b410e43
Hari KT <kthari85@gmail.com>
2015-01-16 21:31:57 +0530

    Structural changes and make use of di

--- web/index.php
+++ web/index.php
@@ -1,10 +1,4 @@
 <?php
-use Aura\Di\Container;
-use Aura\Di\Factory;
-use Aura\Router\Router;
-use Aura\Router\RouteCollection;
-use Aura\Router\RouteFactory;
-use Aura\Router\Generator;
 use Aura\Dispatcher\Dispatcher;
 use Phly\Conduit\Middleware;
 use Phly\Http\Server;
@@ -15,29 +9,22 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 Dotenv::load(dirname(__DIR__));
 
+// Create the object and load the $di
+require dirname(__DIR__) . '/config/di.php';
+
 $dispatcher = new Dispatcher;
 $dispatcher->setObjectParam('controller');
 $dispatcher->setMethodParam('action');
 
-$router = new Router(
-    new RouteCollection(new RouteFactory),
-    new Generator
-);
-
-$di = new Container(new Factory);
-
-require dirname(__DIR__) . '/config/di.php';
 require dirname(__DIR__) . '/config/routes.php';
 require dirname(__DIR__) . '/config/controllers.php';
 
-$routerMiddleware = new RouterMiddleware($router, $dispatcher);
-
-$auth = $di->get('aura/auth:auth');
-$resume_service = $di->get('aura/auth:resume_service');
-$authMiddleware = new AuthenticationMiddleware($auth, $resume_service);
-
 $app = new Middleware();
-$app->pipe('/admin', $authMiddleware);
+$app->pipe('/admin', function ($req, $res, $next) use ($di) {
+    $middleware = $di->newInstance('Conduit\Middleware\AuthenticationMiddleware');
+    $middleware->handle($req, $res, $next);
+});
+$routerMiddleware = new RouterMiddleware($router, $dispatcher);
 $app->pipe($routerMiddleware);
 $server = Server::createServer($app, $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 
